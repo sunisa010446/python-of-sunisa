bb838570da969ef2abf1251cc77a8828e5f08f7e
sunisa010446 <sunisa85551@gmail.com>
2024-07-04 12:14:40 +0700

    ertge

--- you.py
+++ you.py
@@ -1,1 +1,57 @@
-print ('test')
+# age = 60
+# if age > 50 :
+    # print ("ผู้เฒ่า")
+# monny = (int(input("price")))
+# i = 0
+# for i in range(1 , 101):
+    # if i % 2 ==0 :
+        # print(i)
+# if monny == 50 :
+#  print ("coke")
+# elif monny == 60 :
+#  print ("papsy")
+# elif monny == 30 :
+#  print ("lay")
+# elif monny == 100 :
+#  print ("น้ำโกโก้")
+# i = 1
+# while i <= 100 :
+    #  if i % 2 ==1:
+        #  print(i)
+    #  i +=1
+# frineds = ["หมาปั๊ก","ไอ้กอฟ์ท","มอส","ไอ้ปอน","พี่พีท"]
+# for x in frineds:
+    # print(x)
+# def you(suni,sunisa):
+    # print ("เพิ่มเสียง")
+    # print(suni)
+    # print(sunisa)
+
+# def doo():
+    # print ("ลดเสียง")
+
+# def daddy():
+    # print ("ปิดเสียง")
+
+# you(2,10)
+# you(5,10)
+# doo()
+# daddy()
+one = (int(input("number1 : ")))
+two = (int(input("number2 : ")))
+def hoo(one,two):
+    print(one+two)
+
+def hooo(one,two):
+    print(one-two)
+
+def hoooo(one,two):
+    print(one*two) 
+
+def hooooo(one,two):
+    print(one/two)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
+
+hoo(one,two)
+hooo(one,two)
+hoooo(one,two)
+hooooo(one,two)
